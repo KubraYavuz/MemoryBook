3978a2f74655c176eb26411523a92b378e35b336
LENOVO-PC\user <kubreayavuz@gmail.com>
2018-12-20 14:41:30 +0300

    Kullanıcı kayıt ekranı tasarımı ve firebase kontrolü yapıldı.

--- app/src/main/java/com/example/user/memorybook/signinActivity.java
+++ app/src/main/java/com/example/user/memorybook/signinActivity.java
@@ -1,13 +1,56 @@
 package com.example.user.memorybook;
 
+import android.icu.lang.UCharacterEnums;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signinActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    EditText emailText;
+    EditText passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        mAuth = FirebaseAuth.getInstance();
+        emailText=findViewById(R.id.emailText);
+        passwordText=findViewById(R.id.passwordText);
+    }
+    public void signIn(View view)
+    {
+
+
+    }
+    public void signUp(View view)
+    {
+
+        //Kullanıcı girişinin başarılı olması ve olmaması durumu
+        mAuth.createUserWithEmailAndPassword(emailText.getText().toString(),passwordText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(signinActivity.this,"User Created!",Toast.LENGTH_SHORT).show();
+
+                    }
+                }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(signinActivity.this,e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
