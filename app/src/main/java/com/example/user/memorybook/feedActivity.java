a26332bc6589619a292465635dca6107f02aafc3
LENOVO-PC\user <kubreayavuz@gmail.com>
2018-12-20 15:24:34 +0300

    Menü eklendi.

--- app/src/main/java/com/example/user/memorybook/feedActivity.java
+++ app/src/main/java/com/example/user/memorybook/feedActivity.java
@@ -1,9 +1,33 @@
 package com.example.user.memorybook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class feedActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {//Menüyü aktiviteye bağladığımız yer
+
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.add_post,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { //Menü seçilince ne olacağı
+        if(item.getItemId()==R.id.add_post){ //Eğer buna tıklandıysa intent yap
+            Intent intent=new Intent(getApplicationContext(),uploadActivity.class);
+            startActivity(intent);
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
