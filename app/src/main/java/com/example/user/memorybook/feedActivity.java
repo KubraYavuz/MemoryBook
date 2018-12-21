428ecf485345f8c0fa5793c24244af1d3327faf8
Ceylan Ozturk <ceylan.ozturk@ogr.sakarya.edu.tr>
2018-12-21 18:09:27 +0300

    Verileri çekme işlemi yapıldı.

--- app/src/main/java/com/example/user/memorybook/feedActivity.java
+++ app/src/main/java/com/example/user/memorybook/feedActivity.java
@@ -1,6 +1,7 @@
 package com.example.user.memorybook;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,10 +9,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class feedActivity extends AppCompatActivity {
 
@@ -61,6 +66,38 @@ public class feedActivity extends AppCompatActivity {
         adapter=new PostClass(useremailFromFB, usercommentFB,userimageFromFB,this); //İndirdiğim verileri postclass a atıyor
 
         listView.setAdapter(adapter);
+
+    }
+
+    public void getDataFromFirebase(){
+        DatabaseReference newReferance=firebaseDatabase.getReference("Posts");
+        newReferance.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //System.out.println("FBV children: "+ dataSnapshot.getChildren());
+                //System.out.println("FBV key:"+dataSnapshot.getKey());
+                //System.out.println("FBV value: "+dataSnapshot.getValue());
+                //System.out.println("FBV priority: "+dataSnapshot.getPriority());
+
+                for(DataSnapshot ds:dataSnapshot.getChildren()){
+
+                //    System.out.println("FBV ds value"+ds.getValue());
+
+                    HashMap<String,String>hashMap=(HashMap<String, String>) ds.getValue();
+                    System.out.println("FBV useremail:"+hashMap.get("useremail:"));
+
+                    useremailFromFB.add(hashMap.get("useremail"));
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
     }
 }
