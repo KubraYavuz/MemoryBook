98f1192eb4eb786b3e040874be260f2a4f2eb275
Ceylan Ozturk <ceylan.ozturk@ogr.sakarya.edu.tr>
2018-12-24 18:51:04 +0300

    Değişikliğe devam edildi.

--- app/src/main/java/com/example/user/memorybook/MainActivity.java
+++ app/src/main/java/com/example/user/memorybook/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference fNotesDatabase;
 
     @Override
+    // not oluşturma
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
